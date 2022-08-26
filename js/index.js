7a9d66c886cffe03baef676b3f4d210a482706f9
milad8118 <mohammadimilad144@gmail.com>
2022-08-27 00:18:18 +0430

    first step

---
+++ js/index.js
@@ -0,0 +1,10 @@
+function ShowNav(){
+    var showMenu = document.getElementById("drop-down");
+    if ( showMenu.style.display === "block"){
+        showMenu.style.display = "none";
+    }
+    else{
+        showMenu.style.display ="block";
+    }
+
+}
